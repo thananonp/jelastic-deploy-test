94e986c2ad6d513c7e657c85afaf534fa4e7f2a3
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-04-05 21:39:10 +0300

    Before doing HTTPS

--- index.js
+++ index.js
@@ -10,10 +10,23 @@ const cors = require('cors');
 const passport = require('./utils/passportAuth');
 const schemas = require('./schemas/index.js');
 const resolvers = require('./resolvers/index.js');
+const {ValidationError} = require("apollo-server-errors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
+
+const checkAuth = (req, res, next) => {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.redirect('/login'); }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.redirect('/users/' + user.username);
+        });
+    })(req, res, next);
+};
+
 
 (async () => {
     try {
@@ -27,10 +40,27 @@ app.use(cors());
         const server = new ApolloServer({
             typeDefs: schemas,
             resolvers,
+            context: async ({req, res}) => {
+                if (req) {
+                    const user = await checkAuth(req, res);
+                    console.log('app', user);
+                    if (!user) {
+                        throw new ValidationError("Credential not recognized")
+                    } else {
+                        return {
+                            req,
+                            res,
+                            user,
+                        };
+                    }
+                }
+            },
+
         });
 
         app.use('/auth', require('./routes/authRoute'))
-        app.use('/chargemap', passport.authenticate('jwt', {session: false}), require('./routes/chargemapRoute'))
+        app.use('/chargemap', checkAuth, require('./routes/chargemapRoute'))
+        // app.use('/chargemap', passport.authenticate('jwt', {session: false}), require('./routes/chargemapRoute'))
 
         server.applyMiddleware({app});
 
