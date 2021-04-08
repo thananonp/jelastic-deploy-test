2fa00d43c63ef3e86e520ed321a19c99ee42f349
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-04-08 13:47:05 +0300

    Seperate localhost.js and production.js

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 'use strict';
-const https = require('https')
-const http = require('http');
-const fs = require('fs')
+
 require('dotenv').config();
 const mongoose = require("mongoose");
 const {ApolloServer} = require('apollo-server-express')
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('./utils/passportAuth');
 const schemas = require('./schemas/index.js');
 const resolvers = require('./resolvers/index.js');
 const helmet = require('helmet')
+const production = require("./sec/production");
+const {localhost} = require("./sec/localhost");
 const {AuthenticationError} = require("apollo-server-errors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 // app.use(cors());
 
-const sslkey = fs.readFileSync('ssl-key.pem');
-const sslcert = fs.readFileSync('ssl-cert.pem')
-const options = {
-    key: sslkey,
-    cert: sslcert
-};
 
 (async () => {
     try {
@@ -35,6 +29,7 @@ const options = {
             useFindAndModify: false
         });
         console.log('MongoDB connected successfully');
+
 
         const server = new ApolloServer({
             typeDefs: schemas,
@@ -56,25 +51,19 @@ const options = {
             },
 
         });
+        server.applyMiddleware({app});
 
-        https.createServer(options, app).listen(8000)
-        http.createServer((req, res) => {
-            res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
-            res.end();
-        }).listen(3001);
+        process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+        if (process.env.NODE_ENV === 'production') {
+            production(app, 3000);
+        } else {
+            localhost(app, 8000, 3000);
+        }
 
-        app.use ((req, res, next) => {
-            if (req.secure) {
-                // request was via ttps, so do no special handling
-                console.log("secure")
-                next();
-            } else {
-                // request was via http, so redirect to https
-                console.log("normal HTTP")
-                res.redirect('https://' + req.headers.host + req.url);
-            }
-        });
-
+        app.use(helmet({
+            contentSecurityPolicy: false,
+            ieNoOpen: false
+        }))
         app.use('/cors-enabled', cors(), (req, res) => {
             res.json({msg: 'This is CORS-enabled for a Single Route'})
         })
@@ -85,17 +74,13 @@ const options = {
             res.send('Hello Secure World!');
         });
 
-        server.applyMiddleware({app});
 
-        app.listen({port: PORT}, () =>
-            console.log(
-                `GraphQL server =>  https://localhost:${PORT}${server.graphqlPath}`),
-                // `GraphQL server =>  http://localhost:${PORT}${server.graphqlPath}`),
-        );
+        // app.listen({port: PORT}, () =>
+        //     console.log(
+        //         `GraphQL server =>  http://localhost:${PORT}${server.graphqlPath}`),
+        //         // `GraphQL server =>  http://localhost:${PORT}${server.graphqlPath}`),
+        // );
 
-        app.use(helmet({
-            ieNoOpen: false
-        }))
 
     } catch (e) {
         console.log('server error: ' + e.message);
